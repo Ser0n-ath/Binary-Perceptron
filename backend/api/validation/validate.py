ad9567501c02ac496f7252a3bbec5d50225d6546
Ser0n-ath <seron.ath@gmail.com>
2022-01-14 22:15:12 -0500

    -setup validation process, and getting json data

--- backend/api/validation/validate.py
+++ backend/api/validation/validate.py
@@ -1,6 +1,20 @@
 def validate_training_data():
     pass
 
-def validate_test_data():
+def validate_test_data(): #??? 
     pass
 
+
+
+def validate_evaluator_input(req: dict)->bool:
+    #Precondition: Req-> perceptron state, and color to evaluate. 
+    pass
+
+
+#Validate 
+def validate_perceptron_state(perceptron_state: dict) -> bool:
+    #Verify if they are of type float//int
+    pass
+
+def validate_color_selection(color: dict) -> bool:
+    pass
