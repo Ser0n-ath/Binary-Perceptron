7268a06d1b741acdf25f69240afdfc4f94e1f391
Ser0n-ath <seron.ath@gmail.com>
2022-05-01 22:23:12 -0400

    -UI updates when results from backend api recieved

--- backend/api/constants.py
+++ backend/api/constants.py
@@ -7,13 +7,12 @@ PRETRAINED_BLUE = 0.11849347714207387
 PRETRAINED_BIAS = 59.20319004735466
 DEFAULT_LEARNING_RATE = 0.0002
 
-
 #Training/Batch Type: Are we displaying statistics from a default, or user specified training/batch set.
 DEFAULT_FILE = 0
 CUSTOM_FILE = 1
 
 #Pathways
-DEFAULT_PATH_TRAINING_FILE = './api/dataset/training_data.txt'
+DEFAULT_PATH_TRAINING_FILE = './api/dataset/testing_data.txt'
 
 #Training File Information:
 DEFAULT_NUM_TRAINING_FILE = 1000
